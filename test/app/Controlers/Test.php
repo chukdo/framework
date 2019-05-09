8b26f21c68ede7afa8fee6f2e2ac0257ff4b0993
jpd <jp.domingo@gmail.com>
2019-05-09 11:37:30 +0200

    Final router / dispatcher / middleware

--- test/app/Controlers/Test.php
+++ test/app/Controlers/Test.php
@@ -4,10 +4,11 @@ namespace App\Controlers;
 
 use Chukdo\Http\Controler;
 use Chukdo\Http\Response;
+use Chukdo\Json\Input;
 
 class test extends Controler
 {
-    public function index($inputs, $response): Response
+    public function index(Input $inputs, Response $response): Response
     {
         return $response->content('TEST Controler: ' . (string) $inputs);
     }
